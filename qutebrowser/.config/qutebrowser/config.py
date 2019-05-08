6c47e9358f8063eed65bf89c3013456b42db4ed8
Kasper Furnes Bernhoff <kb@kfbi.xyz>
2019-05-08 06:01:01 +0200

    Update i3/.config/i3/config, qutebrowser/.config/qutebrowser/config.py

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,2 +1,12 @@
 c.tabs.position = "bottom"
 c.completion.shrink = True
+c.url.searchengines = {
+   "DEFAULT":"https://google.com/search?q={}",
+   "wa": "https://wiki.archlinux.org/?search={}",
+   "g":"https://github.com/search?q={}",
+   "d":'https://duckduckgo.com/?q={}',
+   "c": 'https://boards.4chan.org/{}/',
+   "r": 'https://www.reddit.com/r/{}',
+   "w": 'https://en.wikipedia.org/wiki/={}',
+   "y": 'https://www.youtube.com/results?search_query={}',
+}

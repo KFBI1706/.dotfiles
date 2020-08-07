102370d87e505c69a64b6561a0451c9901581da0
Kasper Furnes Bernhoff <kb@kfbi.xyz>
2020-08-07 13:37:35 +0200

    screenlayout

---
+++ screenlayout/.screenlayout/desktop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1080 --pos 4920x1620 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x1200 --rotate left --output DP-0 --off --output DVI-D-0 --mode 1920x1080 --pos 2040x0 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 1080x1080 --rotate normal
---
+++ screenlayout/.screenlayout/gaming.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1080 --pos 3000x1080 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x660 --rotate left --output DP-0 --off --output DVI-D-0 --mode 1920x1080 --pos 1080x0 --rotate normal --output DP-1 --primary --mode 1920x1080 --pos 1080x1080 --rotate normal

3543eb9aa0146d7240558697b72f800cb733bc86
Kasper Furnes Bernhoff <kfbi@protonmail.com>
2021-01-06 20:38:35 +0100

    small changes

--- screenlayout/.screenlayout/desktop.sh
+++ screenlayout/.screenlayout/desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1080 --pos 4920x1435 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x1200 --rotate left --output DP-0 --off --output DVI-D-0 --mode 1920x1080 --pos 1822x0 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 1080x1080 --rotate normal
+xrandr --output DP-1 --primary --mode 3840x2160 --pos 1080x1080 --rotate normal --output DP-2 --mode 1920x1080 --pos 1989x0 --rotate normal --output DP-3 --mode 1920x1080 --pos 4920x1682 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x1200 --rotate left --output DVI-I-1-0 --off --output DVI-I-1-1 --off --output HDMI-1-0 --off --output DP-1-0 --off --output DVI-D-1-0 --off --output DP-1-1 --off
